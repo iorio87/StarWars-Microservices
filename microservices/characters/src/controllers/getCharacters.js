c841f7f52563a8a6e483aad32b4412a094694a70
Luciano Petrate <lucianopetrate@gmail.com>
2023-06-29 22:09:18 -0300

    handle error on characters

--- microservices/characters/src/controllers/getCharacters.js
+++ microservices/characters/src/controllers/getCharacters.js
@@ -1,6 +1,7 @@
 const Character = require('../data')
+const {response} = require('../utils')
 
 module.exports = async(req, res) => {
     const characters = await Character.list()
-    res.status(200).json(characters)
+    response(res, 200, characters)
 }
