34328931ae0624729185d7944691204b153b92a9
Luciano Petrate <lucianopetrate@gmail.com>
2023-07-06 19:37:53 -0300

    middleware validation

--- microservices/database/src/routes/index.js
+++ microservices/database/src/routes/index.js
@@ -1,11 +1,17 @@
 const { Router } = require("express");
 const router = Router();
-
 const store = require("../database");
+const { validateModel } = require("../middlewares");
 
-router.get("/:model", async (req, res) => {
+router.get("/:model", validateModel, async (req, res) => {
   const { model } = req.params;
   const response = await store[model].list();
+  res.status(200).json(response);
+});
+
+router.get("/:model/:id", validateModel, async (req, res) => {
+  const { model, id } = req.params;
+  const response = await store[model].get(id);
   res.status(200).json(response);
 });
 
